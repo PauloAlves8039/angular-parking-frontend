316e62f30d8c9cb996deb104a15e1d5676554961
PauloAlves8039 <pj38alves@gmail.com>
2024-08-12 20:20:31 -0300

    feat: created footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule
   ],
-  exports: [NavbarComponent]
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
 })
 export class SharedModule { }
