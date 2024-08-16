9034d22236ecdd487cdd99361c1e86b0bcb7ff79
PauloAlves8039 <pj38alves@gmail.com>
2024-08-16 20:28:52 -0300

    feat: created address routing

---
+++ src/app/modules/address/address.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AddressHomeComponent } from './page/address-home/address-home.component';
+
+export const ADDRESS_ROUTES: Routes = [
+  {
+    path: '',
+    component: AddressHomeComponent,
+  }
+];
