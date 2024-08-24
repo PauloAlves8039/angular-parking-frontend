211efb876424994c9f199c15a31d8e831608f2a8
PauloAlves8039 <pj38alves@gmail.com>
2024-08-24 09:35:55 -0300

    feat: added call vehicle table component

--- src/app/modules/vehicle/vehicle.module.ts
+++ src/app/modules/vehicle/vehicle.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../../shared/shared.module';
 import { VehicleTableComponent } from './components/vehicle-table/vehicle-table.component';
 import { VehicleHomeComponent } from './pages/vehicle-home/vehicle-home.component';
+import { VEHICLE_ROUTES } from './vehicle.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [VehicleTableComponent, VehicleHomeComponent],
@@ -13,6 +15,10 @@ import { VehicleHomeComponent } from './pages/vehicle-home/vehicle-home.componen
     FormsModule,
     HttpClientModule,
     SharedModule,
+    RouterModule.forChild(VEHICLE_ROUTES)
+  ],
+  exports: [
+    VehicleTableComponent
   ]
 })
 export class VehicleModule { }
--- src/app/modules/vehicle/vehicle.routing.ts
+++ src/app/modules/vehicle/vehicle.routing.ts
@@ -1,4 +1,4 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { VehicleHomeComponent } from './pages/vehicle-home/vehicle-home.component';
 
 export const VEHICLE_ROUTES: Routes = [
