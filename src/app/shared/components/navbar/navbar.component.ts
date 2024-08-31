745033d0b9e8f5e39cf72429133a6ae0928977de
PauloAlves8039 <pj38alves@gmail.com>
2024-08-31 12:20:55 -0300

    feat: created sidebar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -10,6 +10,20 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.addSidebarToggleEvent();
+  }
+
+  private addSidebarToggleEvent(): void {
+    const sidebarCollapse = document.getElementById('sidebarCollapse');
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+
+    if (sidebarCollapse && sidebar && content) {
+      sidebarCollapse.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+        content.classList.toggle('active');
+      });
+    }
   }
 
 }
